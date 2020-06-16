7726c270b514eedd8c487c9d1cc46056dc7177b7
Alicerrrrrr <66957031+Alicerrrrrr@users.noreply.github.com>
2020-06-16 01:54:20 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    z
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
+
+
+from flask import render_template
+@app.route("/hello/")
+@app.route("/hello/<name2>")
+def hello_there2(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+
+
+
+
